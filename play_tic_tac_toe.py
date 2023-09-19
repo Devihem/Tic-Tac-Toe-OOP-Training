2484e521b669d48b752158280970a9e23c2c942f
Devihem <Devihem7@gmail.com>
2023-09-19 17:33:28 +0300

    .

--- play_tic_tac_toe.py
+++ play_tic_tac_toe.py
@@ -16,6 +16,7 @@ If no player wins and the board is full, the game is considered a draw.
 
 import random
 from players import Players
+from gaming_board import GamingBoard
 
 
 class PlayTicTacToe:
@@ -25,7 +26,7 @@ class PlayTicTacToe:
 
     def __init__(self):
         self.players = None  # obj
-        self.board = []
+        self.board = GamingBoard()  # obj
 
     @staticmethod
     def welcome_text():
@@ -63,10 +64,6 @@ class PlayTicTacToe:
     # Main method for going through the game phase.
     def playing_phase(self):
 
-        # If there is no more free blocks for next move the game is considerate as draw !
-        max_moves = self.SIZE_OF_GRID ** 2
-        made_moves = 0
-
         # The list of all players ( Human and Ai ) is randomly shuffled. The players turn is based on the list order.
         all_players = self.players.human_players + self.players.ai_players
         random.shuffle(all_players)
@@ -77,7 +74,7 @@ class PlayTicTacToe:
             for player in all_players:
 
                 # Printing the gaming board in the terminal
-                self.board_print()
+                self.board.terminal_print()
 
                 # Future option for AI turns ! Temporary the decision is randomized somewhere on free spot !
                 if player in self.players.ai_players:
@@ -86,58 +83,29 @@ class PlayTicTacToe:
                     row, col = self.human_choose_location(player)
 
                 # Placing the player token on the board
-                self.board[row][col] = player
+                self.board.place_token(row, col, player)
 
-                # Increasing the made moves
-                made_moves += 1
+                # decrease the value of total free cells
+                self.board.free_cells -= 1
 
-                # Winner condition check
-                if self.winner_check():
+                # The method check for winner on the board.
+                if self.board.winner_check():
                     print(f"\n\n--------- We have a winner !---------\n"
                           f"     Player with symbol " + "\033[32m" + f"{player}" + "\033[0m" + " WIN !\n\n")
                     return
 
-                # Draw condition check
-                elif max_moves == made_moves:
+                # If there is no more free cells for next move the game is considerate as draw !
+                elif 0 == self.board.free_cells:
                     print("\n\nNo more moves.\nGame is DRAW !\n\n")
-
                     return
-
-    # Using ASCII symbols and the grid size for different elements to represent the board in the terminal.
-    def board_print(self):
-        list_with_indexes_top = ['']
-
-        for numb in range(self.SIZE_OF_GRID):
-            str_numb = str(numb + 1)
-            str_numb = ' ' * (4 - len(str_numb)) + str_numb
-
-            list_with_indexes_top.append(str_numb)
-
-        # Top frame
-        print('┌──' + '──┬──' * self.SIZE_OF_GRID + '──┐')
-
-        # Index Top
-        print('│    ' + '│'.join(list_with_indexes_top) + '│')
-
-        # Mid Rows
-        counter = 0
-        for row in self.board:
-            counter += 1
-            str_counter = str(counter)
-            str_counter = ' ' * (4 - len(str_counter)) + str_counter
-            print_row = [' ' * (4 - len(el)) + el for el in row]
-            print('├──' + '──┼──' * self.SIZE_OF_GRID + '──┤')
-            print('│' + '│'.join([str_counter] + print_row) + '│')
-
-        # Bottom frame
-        print('└──' + '──┴──' * self.SIZE_OF_GRID + '──┘\n')
 
     # Ai - Location choosing
     def ai_choose_location(self):
         while True:
             row = random.randint(0, self.SIZE_OF_GRID - 1)
             col = random.randint(0, self.SIZE_OF_GRID - 1)
-            if self.board[row][col] == "":
+
+            if self.board.board[row][col] == "":
                 return row, col
 
     # Human - Location choosing with try/expect loop for filtering the incorrect inputs.
@@ -172,7 +140,7 @@ class PlayTicTacToe:
                     raise ValueError
 
                 # Check-4, if the token is placed on free place
-                if self.board[row][col] != "":
+                if self.board.board[row][col] != "":
                     raise ValueError
 
                 # Return the row and col locations if all checks are passed
@@ -181,32 +149,6 @@ class PlayTicTacToe:
             except ValueError:
                 print("Incorrect input! [Expected valid coordinates in format Row:Col where the block is free !]\n\n")
                 continue
-
-    # The method check for winner on the board.
-    def winner_check(self):
-
-        # Rows - Check
-        for row in self.board:
-            if row[0] != '' and all([True if el == row[0] else False for el in row[1:]]):
-                return True
-
-        # Columns - Check
-        for col in range(self.SIZE_OF_GRID):
-            if self.board[0][col] != '' and all(
-                    [True if self.board[0][col] == row[col] else False for row in self.board[1:]]):
-                return True
-
-        # Primer diagonal - Check
-        if self.board[0][0] != '' and all(
-                [True if self.board[0][0] == self.board[row][row] else False for row in range(self.SIZE_OF_GRID)]):
-            return True
-
-        # Secondary diagonal - Check
-        if (self.board[0][self.SIZE_OF_GRID - 1] != ''
-                and all([True if self.board[0][self.SIZE_OF_GRID - 1] == self.board[row][
-                    self.SIZE_OF_GRID - 1 - row] else False
-                         for row in range(self.SIZE_OF_GRID)])):
-            return True
 
     @staticmethod
     def another_game_select():
@@ -247,7 +189,8 @@ class PlayTicTacToe:
 
         while True:
 
-            self.board = [['' for _ in range(self.SIZE_OF_GRID)] for __ in range(self.SIZE_OF_GRID)]
+            # Resize the board based on the Grid Size input ( Create new clear board )
+            self.board.create_empty_board_grid(self.SIZE_OF_GRID)
 
             self.playing_phase()
 
